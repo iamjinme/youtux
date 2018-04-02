a9cefb7570d163321762a0e7e4ea7d910131af54
Jinme Mirabal <mirabalj@gmail.com>
2018-04-01 20:55:04 -0500

    Refactor method to get basic search results

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,20 @@
+import axios from 'axios';
 export const SEARCH_VIDEOS = 'SEARCH_VIDEOS';
 
-export const searchVideos = (text = '') => ({
-  type: SEARCH_VIDEOS,
-  text,
-})
+const API_KEY = 'AIzaSyDK7Jus-yWofaJKGRwDD5gPpZX62t7j47s';
+const YOUTUBE_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&type=video&q=`;
+
+export const searchVideos = (text = '') => {
+  if (!text) return {
+    type: SEARCH_VIDEOS,
+    text,
+    videos: [],
+  }
+  return (dispatch, getState) => {
+    axios.get(`${YOUTUBE_URL}${text}`)
+      .then((response) => {
+        console.log(response.data.items);
+        dispatch({ type: SEARCH_VIDEOS, text, videos: response.data.items })
+      })
+  }
+}
