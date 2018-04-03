66b242da8b6545ce970e5d1fa9f47638796e280c
Jinme Mirabal <mirabalj@gmail.com>
2018-04-03 13:04:49 -0500

    Add icon to main index

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SearchVideos from '../containers/SearchVideos';
 import VisibleVideoList from '../containers/VisibleVideoList';
-import { Grid, Jumbotron } from 'react-bootstrap';
+import { Grid, Glyphicon } from 'react-bootstrap';
 
 const App = () => (
   <div>
     <Grid>
+      <h3><Glyphicon glyph="facetime-video" /> Youtux</h3>
       <SearchVideos />
+      <hr/>
       <VisibleVideoList />
     </Grid>
   </div>
