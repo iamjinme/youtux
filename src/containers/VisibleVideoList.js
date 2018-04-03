9c3756196ce82006d33fc5b8a3bf3d59d8d82703
Jinme Mirabal <mirabalj@gmail.com>
2018-04-03 15:22:09 -0500

    Add filter method to videos by channel state

--- src/containers/VisibleVideoList.js
+++ src/containers/VisibleVideoList.js
@@ -3,9 +3,16 @@ import { connect } from 'react-redux';
 import { saveVideo } from '../actions';
 import VideoList from '../components/VideoList';
 
+const getVisibleVideos = (videos, filterChannel) => {
+  if (filterChannel) {
+    return videos.filter(t => t.user === filterChannel);
+  }
+  return videos;
+}
+
 const mapStateToProps = state => ({
   text: state.textToSearch,
-  videos: state.videos,
+  videos: getVisibleVideos(state.videos, state.filterChannel),
 })
 
 const mapDispatchToProps = dispatch => ({
