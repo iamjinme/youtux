911082a695c27d7f03fce316cf85ec5aea996d9f
Jinme Mirabal <mirabalj@gmail.com>
2018-04-03 15:09:36 -0500

    Add component to manage filter by channel

---
+++ src/components/FilterChannel.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { changeChannel } from '../actions';
+import { Typeahead } from 'react-bootstrap-typeahead';
+
+const FilterChannel = ({ channels, changeChannel }) => (
+  <Typeahead
+    placeholder='Write channel to filter'
+    options = { channels }
+    onChange = { changed => changeChannel(changed[0]) }
+  />
+)
+
+const mapStateToProps = state => ({
+  channels: state.channels,
+})
+
+const mapDispatchToProps = dispatch => ({
+  changeChannel: channel => dispatch(changeChannel(channel))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(FilterChannel);
